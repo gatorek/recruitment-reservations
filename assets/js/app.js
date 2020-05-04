1780cbd7e865a6bfece447f2d3c4e912f0b8b38d
Michał Sacewicz <michal@sacewicz,pl>
2020-05-04 20:54:45 +0200

    added bootstrap and jquery

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,8 +7,11 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import '../css/app.css';
+import '../css/global.scss';
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
-// import $ from 'jquery';
 
-console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+// Bootstrap
+import $ from 'jquery';
+require('bootstrap');
+
