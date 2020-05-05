8f36512306049e198d5aea25ee47199aaa4dc5ab
Michał Sacewicz <michal@sacewicz,pl>
2020-05-05 20:42:18 +0200

    added drag and drop of gear

--- assets/js/components/Workplaces.vue
+++ assets/js/components/Workplaces.vue
@@ -1,30 +1,24 @@
 <template>
-    <div id="wraps">
-        <div class="box box-warning">
-            <div class="box-body">
-                <table id="workplace-list" class="table table-bordered table-hover">
-                    <tr>
-                        <th>Id</th>
-                        <th>Nazwa</th>
-                        <th>Opis</th>
-                        <th>Sprzęt</th>
-                    </tr>
-                    <tr v-for="workplace in workplaces" v-bind:key="workplace.id">
-                        <td>{{ workplace.id }}</td>
-                        <td>{{ workplace.name }}</td>
-                        <td>{{ workplace.description }}</td>
-                        <td>
-                            <ul v-for="gear in workplace.gears" v-bind:key="gear.id">
-                                <li>
-                                    <button type="button" class="btn btn-primary btn-sm gear-edit" :data-gear="gear.id">Edytuj</button>
-                                    <a class="btn btn-danger btn-sm delete" :data-gear="gear.id" href="javascript:console.log(this);" rel="nofollow">Delete</a>
-                                    {{ gear.type }}: {{ gear.name }}
-                                </li>
-                            </ul>
-                            <button type="button" class="btn btn-primary btn-sm gear-add mt-2" :data-workplace="workplace.id">Dodaj sprzęt</button>
-                        </td>
-                    </tr>
-                </table>
+    <div id="workplace-list">
+        <div class="row">
+            <div class="border col-1">Id</div>
+            <div class="border col-3">Nazwa</div>
+            <div class="border col-4">Opis</div>
+            <div class="border col-4">Sprzęt (przeciągnij i upuść, aby przenieść do innego miejsca pracy)</div>
+        </div>
+        <div class="row" v-for="workplace in workplaces" v-bind:key="workplace.id">
+            <div class="border col-1">{{ workplace.id }}</div>
+            <div class="border col-3">{{ workplace.name }}</div>
+            <div class="border col-4">{{ workplace.description }}</div>
+            <div class="border col-4 p-1">
+                <ul class="draggable list-unstyled" :data-workplace="workplace.id">
+                    <li class="border m-1 w-75 p-1" v-for="gear in workplace.gears" v-bind:key="gear.id" :data-gear="gear.ids">
+                        <button type="button" class="btn btn-primary btn-sm gear-edit" :data-gear="gear.id">Edytuj</button>
+                        <a class="btn btn-danger btn-sm delete" :data-gear="gear.id" href="javascript:void(0);" rel="nofollow">Usuń</a>
+                        {{ gear.type }}: {{ gear.name }}
+                    </li>
+                </ul>
+                <button type="button" class="btn btn-primary btn-sm gear-add" :data-workplace="workplace.id">Dodaj sprzęt</button>
             </div>
         </div>
     </div>
@@ -32,7 +26,7 @@
 
 <script>
     export default {
-        props: ['list_url'],
+        props: ['list_url', 'assign_url'],
         data: () => {
             return {
                 workplaces: []
@@ -44,6 +38,7 @@
         },
         updated: function() {
             this.confirmation();
+            this.draggable();
         },
         methods: {
             update: function(){
@@ -57,13 +52,72 @@
             confirmation: function(){
                 $('.delete').confirmation({
                     rootSelector: '.delete',
-                    title: 'na pewno?'
+                    title: 'Na pewno chcesz usunąć?',
+                    btnOkLabel: "Tak",
+                    btnCancelLabel: "Nie"
                 });
+            },
+            draggable: function() {
+                var adjustment;
+                $("ul.draggable").sortable({
+                    group: 'draggable',
+                    pullPlaceholder: false,
+                    onDrop: ($item, container, _super) => {
+                        var $clonedItem = $('<li/>').css({height: 0});
+                        $item.before($clonedItem);
+                        $clonedItem.animate({'height': $item.height()});
+
+                        $item.animate($clonedItem.position(), function  () {
+                            $clonedItem.detach();
+                            _super($item, container);
+                        });
+
+                        $.ajax({
+                            url: this.assign_url
+                                .replace('/0/', `/${$item[0].firstChild.dataset.gear}/`)
+                                .slice(0, -1)
+                                + container.el[0].dataset.workplace,
+                            method: "POST",
+                            success: (data) => {
+                                $root.$refs.workplaces.update();
+                            },
+                            error: () => {
+                                alert('Nie udało się przenieść sprzętu do innego miejsca pracy')
+                            }
+                        });
+                    },
+                    // set $item relative to cursor position
+                    onDragStart: function ($item, container, _super) {
+                        var offset = $item.offset(),
+                            pointer = container.rootGroup.pointer;
+
+                        adjustment = {
+                            left: pointer.left - offset.left,
+                            top: pointer.top - offset.top
+                        };
+
+                        _super($item, container);
+                    },
+                    onDrag: function ($item, position) {
+                        $item.css({
+                            left: position.left - adjustment.left,
+                            top: position.top - adjustment.top
+                        });
+                    }
+                });
+
             }
         }
     }
 </script>
 
 <style scoped>
-
+    body.dragging, body.dragging * {
+        cursor: move !important;
+    }
+    .dragged {
+        position: absolute;
+        opacity: 0.5;
+        z-index: 2000;
+    }
 </style>
