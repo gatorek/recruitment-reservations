e31ae69c3f74e422ac1027beaf67aa5a366fb971
Michał Sacewicz <michal@sacewicz,pl>
2020-05-05 08:24:03 +0200

    added vuejs

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,3 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
-
 // any CSS you import will output into a single css file (app.css in this case)
 import '../css/app.css';
 import '../css/global.scss';
@@ -14,3 +7,20 @@ const $ = require('jquery');
 global.$ = global.jQuery = $;
 require('bootstrap');
 
+// VueJS
+import Vue from 'vue';
+import moment from 'moment'
+import Reservations from './components/Reservations'
+
+Vue.filter('time', function(value) {
+    if (value) {
+        return moment(String(value)).format('YYYY-MM-DD HH:mm')
+    }
+});
+
+let $root = new Vue({
+    el: '#content-main',
+    components: {Reservations}
+});
+
+export default { $root };
